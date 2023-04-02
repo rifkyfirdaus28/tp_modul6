fb8e44c10aa1de48e88bf66ea79d0ccece50da67
rifkyfirdaus28 <rifkyfrds@student.telkomuniversity.ac.id>
2023-04-03 01:55:14 +0700

    Add project files.

---
+++ TP_MOD6_1302204112/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_MOD6_1302204112
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
